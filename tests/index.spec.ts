fe807b436067a007ca0db5705de5152d26c488b9
Andreas Heissenberger <andreas@heissenberger.at>
2022-01-28 01:40:07 +0100

    update packages

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -14,15 +14,15 @@ describe('configuration has', () => {
         fsmock(
             {
                 'loader.js': 'A',
-                'lib.js':'const a=1; a=2;',
-                'path/to/fake/date.js':'_2020-02-01_'
+                'lib.js': 'const a=1; a=2;',
+                'path/to/fake/date.js': '_2020-02-01_'
             }
         )
     })
 
     afterEach(fsmock.restore);
 
-    it('is minimal pattern', async (doneTest) => {
+    it('is minimal pattern', (doneTest) => {
         const setup: optionI = {
             pattern: [
                 ['A', 'B']
@@ -51,7 +51,7 @@ describe('configuration has', () => {
         }).toThrow();
     })
 
-    it('regex pattern', async (doneTest) => {
+    it('regex pattern', (doneTest) => {
         const setup: optionI = {
             pattern: [
                 [/(\s*)const(\s+a\s*=\s*1[\s;\n])/g, '$1let$2']
@@ -68,12 +68,12 @@ describe('configuration has', () => {
 
     })
 
-    it('function pattern', async (doneTest) => {
+    it('function pattern', (doneTest) => {
         const setup: optionI = {
             include: /fake\/date\.js$/,
             pattern: [
-                
-                [/(\d{4})-(\d{2})-(\d{2})/g , (match,p1,p2,p3,offset,wholeString)=>`${p3}.${p2}.${p1}`]
+
+                [/(\d{4})-(\d{2})-(\d{2})/g, (match, p1, p2, p3, offset, wholeString) => `${p3}.${p2}.${p1}`]
             ]
         }
         const expected = '_01.02.2020_'
